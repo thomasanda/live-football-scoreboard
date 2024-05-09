7c610302d0f0d36483d8d84e3c5cbcb973d98556
Thomas Anda <thomas.anda@oliasoft.com>
2024-05-09 23:50:06 +0200

    Fix scoreboard tests

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -5,19 +5,23 @@ describe("Scoreboard", () => {
     const scoreboard = new Scoreboard();
     scoreboard.startMatch("England", "Norway");
     scoreboard.startMatch("Germany", "Spain");
-    const matches = scoreboard.getMatches();
+    const matches = scoreboard.getMatchesSummary();
     expect(matches.length).toBe(2);
   });
 
   test("add new matches to scoreboard and update score", () => {
     const scoreboard = new Scoreboard();
-    scoreboard.startMatch("England", "Norway");
+    const match1 = scoreboard.startMatch("England", "Norway");
     const match2 = scoreboard.startMatch("Germany", "Spain");
-    match2.updateScore(1, 0);
+    match1.updateScore(1, 4);
+    match2.updateScore(0, 1);
     match2.updateScore(1, 1);
-    const matches = scoreboard.getMatches();
-    const expected = "Germany 1 - 1 Spain";
-    expect(matches[1].getScore()).toEqual(expected);
+    match2.updateScore(2, 1);
+    match2.updateScore(3, 1);
+    match2.updateScore(4, 1);
+    const matches = scoreboard.getMatchesSummary();
+    const expected = ["England 1 - 4 Norway", "Germany 4 - 1 Spain"];
+    expect(matches).toEqual(expected);
   });
 
   test("remove match from scoreboard", () => {
@@ -26,7 +30,7 @@ describe("Scoreboard", () => {
     scoreboard.startMatch("Haugesund", "Lillestrøm");
     scoreboard.startMatch("Rosenborg", "Start");
     scoreboard.finishMatch(match);
-    const matches = scoreboard.getMatches();
+    const matches = scoreboard.getMatchesSummary();
     expect(matches.length).toBe(2);
   });
 });
