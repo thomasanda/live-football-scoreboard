6a09c41784166507483362d65d6e010c8031eb88
Thomas Anda <thomas.anda@oliasoft.com>
2024-05-09 23:49:17 +0200

    Add starttime and totalscore props to match class

--- src/match.ts
+++ src/match.ts
@@ -3,12 +3,16 @@ class Match {
   private awayTeam: string;
   private homeScore: number;
   private awayScore: number;
+  private startTime: Date;
+  private totalScore: number;
 
   constructor(homeTeam: string, awayTeam: string) {
     this.homeTeam = homeTeam;
     this.awayTeam = awayTeam;
     this.homeScore = 0;
     this.awayScore = 0;
+    this.startTime = new Date();
+    this.totalScore = 0;
   }
 
   public getScore(): string {
@@ -18,6 +22,15 @@ class Match {
   public updateScore(homeScore: number, awayScore: number): void {
     this.homeScore = homeScore;
     this.awayScore = awayScore;
+    this.totalScore = homeScore + awayScore;
+  }
+
+  public getStartTime(): Date {
+    return this.startTime;
+  }
+
+  public getTotalScore(): number {
+    return this.totalScore;
   }
 }
 
